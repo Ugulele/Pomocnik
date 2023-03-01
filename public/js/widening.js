5d8f269365ccbc62986b580be47e7557dd7a8aa5
Ugulele <adamczyk.blazej.96@gmail.com>
2023-03-01 23:44:51 +0100

    dodanie nowych funkcji do turns.js i widening.js

--- public/js/widening.js
+++ public/js/widening.js
@@ -31,9 +31,10 @@ function runliczPoszerzenie(){
     let alfa = document.getElementById("widening_angle").value;
     let l1 = document.getElementById("widening_curve_length_1").value;
     let l2 = document.getElementById("widening_curve_length_2").value;
+    let curvetype = document.getElementById("widening_curvetype").value;
 
     if(r1 != 0 && r2 != 0 && d != 0 && alfa != 0){
-        liczPoszerzenie(r1,r2,d,alfa,l1,l2);
+        liczPoszerzenie(r1,r2,d,alfa,l1,l2,curvetype);
     }else if(r1 == 0 && r2 == 0 && d == 0 && alfa == 0 ){
         document.getElementById("widening_inset").value = "";
         document.getElementById("widening_length").value = "";
@@ -44,67 +45,82 @@ function runliczPoszerzenie(){
     
 }
 
-function liczPoszerzenie(r1,r2,d,alfa,l1,l2){
+function liczPoszerzenie(r1,r2,d,alfa,l1,l2,curvetype){
     //obliczanie wartości wstawki i długości i przesunięć
     let w = d/(Math.sin(radians(alfa))) - r1*Math.tan(radians(alfa/2)) - r2*Math.tan(radians(alfa/2)) - l1/2 - l2/2;
-
+    let n1 = liczPrzesuniecie(r1,l1,curvetype);
+    let n2 = liczPrzesuniecie(r2,l2,curvetype);
+   
     //wyświetlanie wyników
     document.getElementById("widening_inset").value = cround(w,4);
     document.getElementById("widening_length").value = cround(w + 2*l1 + 2*l2 + (alfa/360)*2*Math.PI*((r1)) + (alfa/360)*2*Math.PI*((r2)),4);
     document.getElementById("widening_curve1").value = cround((alfa/360)*2*Math.PI*((r1)) - l1,4);
     document.getElementById("widening_curve2").value = cround((alfa/360)*2*Math.PI*((r2)) - l2,4);
+    document.getElementById("widening_shift_1").value = cround(n1,2);
+    document.getElementById("widening_shift_2").value = cround(n2,2);
 }
 
 function runliczPoszerzenie2(){
     let r = document.getElementById("widening2_radius").value;
     let l = document.getElementById("widening2_curve_length").value;
     let d = document.getElementById("widening2_change").value;
+    let curvetype = document.getElementById("widening2_curvetype").value;
 
     if(r != 0 && d != 0){
-        liczPoszerzenie2(r,l,d);
+        liczPoszerzenie2(r,l,d,curvetype);
     }else if(r == 0 || d == 0){
         document.getElementById("widening2_length").value = "";
         document.getElementById("widening2_angle").value = "";
         document.getElementById("widening2_curve").value = "";
-        
-        
+        document.getElementById("widening2_shift").value = "";
     }
 }
 
-function liczPoszerzenie2(r,l,d){
+function liczPoszerzenie2(r,l,d,curvetype){
     //Obliczanie poszerzenia
     let k = (-3*l +  Math.sqrt(l**2 + 4*r*d))/2;
     //Obliczanie kąta łuku
     let alfa = degrees(2*Math.atan((k+l)/(2*r)));
     //Obliczanie długości poszerzenia
     let p = 2*k + 4*l;
+    //obliczanie przesunięcia
+    let n = liczPrzesuniecie(r,l,curvetype);
+  
+    
+
     //wyświetlanie wyników
     document.getElementById("widening2_length").value = cround(p,2);
     document.getElementById("widening2_angle").value = cround(alfa,4);
     document.getElementById("widening2_curve").value = cround((alfa/360)*2*Math.PI*((r)),4);
+    document.getElementById("widening2_shift").value = cround(n,2);
     
 }
 
 function runliczPoszerzenie3(){
-    r = document.getElementById("widening3_radius").value;
-    d = document.getElementById("widening3_change").value;
+    let r = document.getElementById("widening3_radius").value;
+    let d = document.getElementById("widening3_change").value;
+    let curvetype = document.getElementById("widening3_curvetype").value;
 
     if(r !=0 && d != 0){
-        liczPoszerzenie3(r,d)
+        liczPoszerzenie3(r,d,curvetype)
     }else if(r == 0 || d == 0){
         document.getElementById("widening3_length").value = "";
         document.getElementById("widening3_curve_length").value = "";
+        document.getElementById("widening3_shift").value = "";
     }
 }
 
-function liczPoszerzenie3(r,d){
-    //obliczanie promienia krzywej
+function liczPoszerzenie3(r,d,curvetype){
+    //obliczanie długości krzywej
     l = cround(Math.sqrt(d*(8*r-d))/4,2)
     p = cround(4*l,2)
+
+    n = liczPrzesuniecie(r,l,curvetype);
 
     //wyprowadzenie wyników
     document.getElementById("widening3_length").value = p;
     document.getElementById("widening3_curve_length").value = l;
+    document.getElementById("widening3_shift").value = cround(n,2);
 }
 
 function runliczPoszerzenieluk(){
